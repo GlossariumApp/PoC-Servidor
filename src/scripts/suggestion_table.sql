f217fcca8e391f141bd91437b1fe761c8b717e6a
João Roberto <joao.roberto@ufv.br>
2026-03-15 22:14:39 -0300

    fix: correção no script sql

--- src/scripts/suggestion_table.sql
+++ src/scripts/suggestion_table.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS suggestions (
     id INT NOT NULL AUTO_INCREMENT,
-    word_id NOT NULL,
+    word_id INT NOT NULL,
     dictionary VARCHAR(30) NOT NULL,
     new_definition VARCHAR(500) NOT NULL,
     justification VARCHAR(500),
     source VARCHAR(500) NOT NULL,
     status VARCHAR(10) NOT NULL,
-    time_created NOT_NULL,
+    time_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
 );
